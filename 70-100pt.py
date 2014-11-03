53e55a743483492ab9ef78703eec6d9827f5a90e
adhowa <aubreyhowa@gmail.com>
2014-11-03 15:37:25 -0600

    finished 100pt

--- 70-100pt.py
+++ 70-100pt.py
@@ -19,6 +19,14 @@ player = drawpad.create_oval(390,580,410,600, fill="red")
 
 # Create your "enemies" here, before the class
 
+circle = drawpad.create_oval(10,10,50,50, fill='green')
+direction = 1
+
+enimy1 = drawpad.create_oval(100,100,50,50, fill='green')
+direction = 1 
+
+enimy2 = drawpad.create_oval(200,200,50,50, fill='green')
+direction = 1 
 
 class MyApp:
 	def __init__(self, parent):
@@ -26,6 +34,7 @@ class MyApp:
        	    self.myParent = parent  
        	    self.myContainer1 = Frame(parent)
        	    self.myContainer1.pack()
+       	    
        	    self.up = Button(self.myContainer1)
        	    self.up.configure(text="up", background= "green")
        	    self.up.grid(row=0,column=2)
@@ -49,13 +58,33 @@ class MyApp:
        	    self.right.bind("<Button-1>",self.rightClicked)
        	    # No need to edit this - just includes the drawpad into our frame
        	    drawpad.pack(side=BOTTOM)
+       	    self.animate()
        	    
 	def animate(self):
 	    global drawpad
 	    global player
+	    global circle
 	    # Remember to include your "enemies" with "global"
-	    	
-		
+            x1, y1, x2, y2 = drawpad.coords(circle)
+            if x2 > drawpad.winfo_width(): 
+                drawpad.move(circle, -800, 0)
+            elif x1 < 0:
+                direction = 5
+            drawpad.move(circle,20,0)
+            x1, y1, x2, y2 = drawpad.coords(enimy1)
+            if x2 > drawpad.winfo_width():
+                drawpad.move(enimy1,-800, 0)
+            elif x1 <0:
+                direction = 5
+            drawpad.move(enimy1,10,0)
+            x1, y1, x2, y2 = drawpad.coords(enimy2)
+            if x2 > drawpad.winfo_width():
+                drawpad.move(enimy2,-800, 0)
+            elif x1 <0:
+                direction = 5 
+            drawpad.move(enimy2,5,0)
+            drawpad.after(1, self.animate)  
+     	
 	def upClicked(self, event):   
 	   global oval
 	   global player
